50977a46eea2124ad30fbc6fcee9316f2c6728f8
gabrieltdrk <gabriel.andradesx@gmail.com>
2024-10-16 14:08:51 -0300

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import { HomePage } from "./pages/Homepage"
+
+export function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,43 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import homePageImage from '../assets/img_login.png'
+
+export function HomePage() {
+    return (
+        <section className="flex gap-x-40">
+            <img src={homePageImage} className="h-screen" />
+            <div className="flex gap-2 flex-col justify-center items-center w-[40%]">
+                <h3>Entre no Orange Portfólio</h3>
+                <button className="">Entrar com Google</button>
+                <h4>Faça login com email</h4>
+                <TextField
+                    className='w-full'
+                    required
+                    id="outlined-basic"
+                    label='Email address'
+                />
+
+                <TextField
+                    className='w-full'
+                    required
+                    id="outlined-basic"
+                    label='Password'
+                />
+
+                <Button
+                    className='w-full'
+                    variant='contained'
+                    color=''
+                > Entrar
+                </Button>
+
+                <Link 
+                    className="self-start"
+                    href="#" 
+                    underline="none">Cadastre-se</Link>
+            </div>
+            
+        </section>
+    )
+}
