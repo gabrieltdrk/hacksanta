cc10811abe31a9e9ff80e75eb2602315aa15c1da
gabrieltdrk <gabriel.andradesx@gmail.com>
2024-10-17 09:28:48 -0300

    gridportfolio modified

--- src/components/GridPortfolio.jsx
+++ src/components/GridPortfolio.jsx
@@ -1,15 +1,15 @@
 export function GridPortfolio() {
-    return(
-        <main className="grid">
-            <div>
+    return (
+        <main className="grid grid-cols-1 lg:grid-cols-3 items-center justify-center gap-8">
+            <div className="w-[312px] h-[258px] bg-black mx-auto">
                 projeto 1
             </div>
-            <div>
+            <div className="w-[312px] h-[258px] bg-black mx-auto">
                 projeto 2
             </div>
-            <div>
+            <div className="w-[312px] h-[258px] bg-black mx-auto">
                 projeto 3
             </div>
         </main>
-    )
+    );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,24 +4,27 @@ import avatarF from '../assets/avatar_f.png';
 export function Profile() {
     return (
         <section className="flex flex-col gap-4 items-center justify-center my-12">
-            <Avatar
-                alt="Usuário"
-                src={avatarF}
-                sx={{ width: 122, height: 122 }}
-            />
-            <div className="flex flex-col gap-1">
-                <h1 className="text-2xl">Gabriel Andrade</h1>
-                <span className="">Brasil</span>
-                <Button
-                    variant="outlined"
-                    sx={{
-                        fontWeight: 'bold',
-                        border: '1px solid #ddd',
-                        textTransform: 'none',
-                        color: '#00000088',
-                    }}>
-                    Adicionar projeto
-                </Button>
+            <div className="flex flex-col md:flex-row gap-4 lg:gap-8 items-center">
+                <Avatar
+                    alt="Usuário"
+                    src={avatarF}
+                    sx={{ width: 122, height: 122 }}
+                />
+                <div className="flex flex-col gap-1">
+                    <h1 className="text-2xl">Gabriel Andrade</h1>
+
+                    <h2 className="text-sm">Brasil</h2>
+                    <Button
+                        variant="outlined"
+                        sx={{
+                            fontWeight: 'bold',
+                            border: '1px solid #ddd',
+                            textTransform: 'none',
+                            color: '#00000088',
+                        }}>
+                        Adicionar projeto
+                    </Button>
+                </div>
             </div>
         </section>
     )
